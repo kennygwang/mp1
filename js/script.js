772f0b6290c38b54ee4bc0c713dd28400c0a26f3
Kenny G. Wang <kennygwang@gmail.com>
2015-02-13 02:59:08 -0600

    working on a bunch of stuff

--- js/script.js
+++ js/script.js
@@ -14,3 +14,36 @@ function highlightThis(event) {
     alert(this.className);
     this.style.backgroundColor=backgroundColor;
 }
+
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+    var directoryItems = $("#directory li"),
+        scrollTop = $(window).scrollTop();
+
+    // Navbar Resizing
+    if (scrollTop <= 0) {
+      $("header").addClass("larger-header");
+    } else {
+      $("header").removeClass("larger-header");
+    }
+
+    // Position Indicator
+    if (scrollTop < 550) {
+      directoryItems.removeClass("active");
+      $("#section-1-nav").addClass("active");
+    } else if (scrollTop < 1000) {
+      directoryItems.removeClass("active");
+      $("#section-2-nav").addClass("active");
+    } else if (scrollTop + $(window).height() === document.body.offsetHeight) {
+      directoryItems.removeClass("active");
+      $("#section-4-nav").addClass("active");
+    } else if (scrollTop < 1450) {
+      directoryItems.removeClass("active");
+      $("#section-3-nav").addClass("active");
+    }
+
+
+
+  });
+});
