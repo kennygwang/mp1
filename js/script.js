1b053e76e9447e3fb690c844ce639176d20ac6a3
Kenny G. Wang <kennygwang@gmail.com>
2015-02-14 16:16:37 -0600

    fixed annoying bugs

--- js/script.js
+++ js/script.js
@@ -23,68 +23,69 @@ $(document).ready(function(){
     } else if (scrollTop < 1400) {
       directoryItems.removeClass("active");
       $("#section-3-nav").addClass("active");
-  }
-
-  $("#section-1-nav").click(function(){
-    $('html,body').animate({
-      scrollTop: $("#section-1").offset().top
-    }, 1000);
+    }
   });
 
-  $("#section-2-nav").click(function(){
+  $("#section-1-nav").click(function(){
+    console.log("navigating to section 1");
+    $('html,body').animate({
+      scrollTop: $("#section-1").offset().top
+    }, 500);
+  });
+
+  $("#section-2-nav").click(function(e){
+    console.log("navigating to section 2");
     $('html,body').animate({
       scrollTop: $("#section-2").offset().top
-    }, 1000);
+    }, 500);
   });
 
   $("#section-3-nav").click(function(){
+    console.log("navigating to section 3");
     $('html,body').animate({
       scrollTop: $("#section-3").offset().top
     }, 500);
   });
 
   $("#section-4-nav").click(function(){
+    console.log("navigating to section 4");
     $('html,body').animate({
       scrollTop: $("#section-4").offset().top
     }, 500);
   });
 
-  var cimages = $(".cimg");
   var cimg1 = $("#cimg1");
   var cimg2 = $("#cimg2");
   var cimg3 = $("#cimg3");
-  $("#cright").click(function(){
+  $("#cright").click(function(e){
     if (cimg1.is(":visible")) {
-      cimg1.animate({height:'toggle'},350);
-      cimg2.animate({height:'toggle'},350);
+      cimg1.animate({width:'toggle'},350);
+      cimg2.animate({width:'toggle'},350);
       return;
     } else if (cimg2.is(":visible")) {
-      cimg2.animate({height:'toggle'},350);
-      cimg3.animate({height:'toggle'},350);
+      cimg2.animate({width:'toggle'},350);
+      cimg3.animate({width:'toggle'},350);
       return;
     } else if (cimg3.is(":visible")) {
-      cimg3.animate({height:'toggle'},350);
-      cimg1.animate({height:'toggle'},350);
+      cimg3.animate({width:'toggle'},350);
+      cimg1.animate({width:'toggle'},350);
       return;
     }
   });
 
   $("#cleft").click(function(){
     if (cimg1.is(":visible")) {
-      cimg1.animate({height:'toggle'},350);
-      cimg3.animate({height:'toggle'},350);
+      cimg1.animate({width:'toggle'},350);
+      cimg3.animate({width:'toggle'},350);
       return;
     } else if (cimg2.is(":visible")) {
-      cimg2.animate({height:'toggle'},350);
-      cimg1.animate({height:'toggle'},350);
+      cimg2.animate({width:'toggle'},350);
+      cimg1.animate({width:'toggle'},350);
       return;
     } else if (cimg3.is(":visible")) {
-      cimg3.animate({height:'toggle'},350);
-      cimg2.animate({height:'toggle'},350);
+      cimg3.animate({width:'toggle'},350);
+      cimg2.animate({width:'toggle'},350);
       return;
     }
-  });
-
-
   });
 });
