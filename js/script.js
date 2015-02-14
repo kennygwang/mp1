6f1d773772b082fd4a5f4321a0c7f1688443da29
Kenny G. Wang <kennygwang@gmail.com>
2015-02-13 19:45:00 -0600

    implemented carousel

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
-
-function highlightThis(event) {
-    //event.stopPropagation();
-  
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
-
 $(document).ready(function(){
-
   $(window).scroll(function(){
     var directoryItems = $("#directory li"),
         scrollTop = $(window).scrollTop();
@@ -29,20 +11,79 @@ $(document).ready(function(){
     }
 
     // Position Indicator
-    if (scrollTop < 550) {
+    if (scrollTop < 500) {
       directoryItems.removeClass("active");
       $("#section-1-nav").addClass("active");
-    } else if (scrollTop < 1000) {
+    } else if (scrollTop < 950) {
       directoryItems.removeClass("active");
       $("#section-2-nav").addClass("active");
     } else if (scrollTop + $(window).height() === document.body.offsetHeight) {
       directoryItems.removeClass("active");
       $("#section-4-nav").addClass("active");
-    } else if (scrollTop < 1450) {
+    } else if (scrollTop < 1400) {
       directoryItems.removeClass("active");
       $("#section-3-nav").addClass("active");
-    }
+  }
 
+  $("#section-1-nav").click(function(){
+    $('html,body').animate({
+      scrollTop: $("#section-1").offset().top
+    }, 1000);
+  });
+
+  $("#section-2-nav").click(function(){
+    $('html,body').animate({
+      scrollTop: $("#section-2").offset().top
+    }, 1000);
+  });
+
+  $("#section-3-nav").click(function(){
+    $('html,body').animate({
+      scrollTop: $("#section-3").offset().top
+    }, 500);
+  });
+
+  $("#section-4-nav").click(function(){
+    $('html,body').animate({
+      scrollTop: $("#section-4").offset().top
+    }, 500);
+  });
+
+  var cimages = $(".cimg");
+  var cimg1 = $("#cimg1");
+  var cimg2 = $("#cimg2");
+  var cimg3 = $("#cimg3");
+  $("#cright").click(function(){
+    if (cimg1.is(":visible")) {
+      cimg1.animate({height:'toggle'},350);
+      cimg2.animate({height:'toggle'},350);
+      return;
+    } else if (cimg2.is(":visible")) {
+      cimg2.animate({height:'toggle'},350);
+      cimg3.animate({height:'toggle'},350);
+      return;
+    } else if (cimg3.is(":visible")) {
+      cimg3.animate({height:'toggle'},350);
+      cimg1.animate({height:'toggle'},350);
+      return;
+    }
+  });
+
+  $("#cleft").click(function(){
+    if (cimg1.is(":visible")) {
+      cimg1.animate({height:'toggle'},350);
+      cimg3.animate({height:'toggle'},350);
+      return;
+    } else if (cimg2.is(":visible")) {
+      cimg2.animate({height:'toggle'},350);
+      cimg1.animate({height:'toggle'},350);
+      return;
+    } else if (cimg3.is(":visible")) {
+      cimg3.animate({height:'toggle'},350);
+      cimg2.animate({height:'toggle'},350);
+      return;
+    }
+  });
 
 
   });
